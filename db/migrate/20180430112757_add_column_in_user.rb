3136c7a49f0bd901bc4540045aa0f2b977c88e19
ANAS AHMED <ahmedanas9312@ gmail.com>
2018-07-18 13:07:25 +0530

    delete migration

--- db/migrate/20180430112757_add_column_in_user.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnInUser < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :users, :first_name, :string
-  	add_column :users, :last_name, :string
-  	add_column :users, :description, :string
-  end
-end
